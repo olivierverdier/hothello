006c2f3e9d3fbe20cd528af5dc0d71d82e0b355f
Olivier Verdier <olivier.verdier@gmail.com>
2017-10-27 10:58:52 +0200

    split move function in two

--- src/Lib.hs
+++ src/Lib.hs
@@ -127,16 +127,18 @@ swap coord board = newBoard mPlayer where
   newBoard (Just p) = Map.insert coord p board
 
 
-move :: Player -> Coordinate -> Board -> Maybe Board
-move me coord board = result where
+tryMove :: Player -> Coordinate -> Board -> Maybe Board
+tryMove me coord board = result where
   gathered = gatherAllEnemyCells board me coord
-  illegalMove = null gathered || isJust (Map.lookup coord board)
+  illegalMove = null gathered
   swapped = foldr swap board gathered
   newBoard = Map.insert coord me swapped
   result = if illegalMove then Nothing else Just newBoard
 
-
-
+move :: Player -> Coordinate -> Board -> Maybe Board
+move me coord board = result where
+  illegalMove = isJust (Map.lookup coord board)
+  result = if illegalMove then Nothing else tryMove me coord board
 
 
 
