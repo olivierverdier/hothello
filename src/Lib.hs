a9aae487a1f1aa7dde46ee5147822b981d9cf215
Olivier Verdier <olivier.verdier@gmail.com>
2017-10-29 12:25:18 +0100

    refactor in smaller functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,7 @@ module Lib
     ( someFunc
     ) where
 
-import Data.Maybe (isJust)
+import Data.Maybe (isNothing)
 
 
 import Player
@@ -14,28 +14,28 @@ import qualified Data.Map.Strict as Map
 
 
 
--- play :: Board -> Coordinate -> Board
 
+swapGathered :: [Coordinate] -> Board -> Board
+swapGathered gathered board = foldr swap board gathered
 
+-- rules
 
+allowFromGathered :: [Coordinate] -> Bool
+allowFromGathered = not . null
+
+allowFromCell :: Cell -> Bool
+allowFromCell = isNothing
+
+addPieceAt :: Player -> Coordinate -> Board -> Board
+addPieceAt me coord = Map.insert coord me
 
 tryMove :: Player -> Coordinate -> Board -> Maybe Board
 tryMove me coord board = result where
   gathered = gatherAllEnemyCells board me coord
-  illegalMove = null gathered
-  swapped = foldr swap board gathered
-  newBoard = Map.insert coord me swapped
-  result = if illegalMove then Nothing else Just newBoard
-
-move :: Player -> Coordinate -> Board -> Maybe Board
-move me coord board = result where
-  illegalMove = isJust (Map.lookup coord board)
-  result = if illegalMove then Nothing else tryMove me coord board
-
-
-
- 
-
+  current = Map.lookup coord board
+  legal = allowFromCell current && allowFromGathered gathered
+  newBoard = addPieceAt me coord board
+  result = if legal then Just newBoard else Nothing
 
 
 
