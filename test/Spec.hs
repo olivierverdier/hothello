9d14e06b4a253ef7b111b61ba10af148d0fb527f
Olivier Verdier <olivier.verdier@gmail.com>
2017-10-29 14:08:45 +0100

    shorter test

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,9 +13,6 @@ board6 = putColAt Black 1 [1..3] $ putColAt White 2 [1..3] emptyBoard
 board6_13 :: Board
 board6_13 = putBlack (MakeCoordinate 1 3) $ putColAt Black 2 [1..2]  board6
 
-play34 :: Maybe Board
-play34 = tryMove White (MakeCoordinate 3 4) startBoard
-
 expected34 :: Board
 expected34 = putColAt White 4 [3..4] startBoard
 
@@ -23,7 +20,7 @@ expected34 = putColAt White 4 [3..4] startBoard
 unitTests :: TestTree
 unitTests = testGroup "HUnit tests" [
   testCase "First legal turn" $
-  play34 @?= Just expected34,
+  tryMove White (MakeCoordinate 3 4) startBoard @?= Just expected34,
   testCase "Black 1 3 after board 6" $
   tryMove Black (MakeCoordinate 1 3) board6 @?= Just board6_13,
   testCase "Illegal empty move" $
