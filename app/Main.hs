06307c18ca0821d85d7f383ee9cc2b307555781e
Olivier Verdier <olivier.verdier@gmail.com>
2017-12-25 20:20:24 +0100

    fully use state transformer
    no more runState kludge

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,29 @@
 module Main where
 
+import Player (Player(Black))
 import Game
 import Board
-import Control.Monad.State.Lazy (get, gets, put, StateT, liftIO, runStateT)
+import Control.Monad.State.Lazy (get, put, StateT, liftIO, runStateT, lift)
 import Input
 
 
-reaction :: (Monad m) => Maybe Input -> StateT Game m String
+reaction :: (Monad m) => Maybe Input -> StateT Player (StateT Board m) String
 reaction Nothing = return "??"
 reaction (Just (Left _)) = do
   _ <- switchPlayer
   return "pass"
 reaction (Just (Right c)) = do
-  before <- get
+  currentPlayer <- get
+  currentBoard <- lift get
   legal <- play c
-  board <- gets getBoard
-  decision legal board before where
-    decision False _ before = do
-      put before -- return to previous state
+  board <- lift get
+  if legal
+    then
+      return (printStdBoard board)
+    else do
+      put currentPlayer
+      lift (put currentBoard) -- return to previous state
       return "illegal move"
-    decision True board _ = return (printStdBoard board)
 
 
 getMove :: IO (Maybe Input)
@@ -34,9 +38,11 @@ prompt (MkGame board player) =
     print player
     putStrLn (printStdBoard board)
 
-gameSession :: StateT Game IO ()
+gameSession :: StateT Player (StateT Board IO) ()
 gameSession = do
-  game <- get
+  player <- get
+  board <- lift get
+  let game = MkGame board player
   _ <- liftIO (prompt game)
   mi <- liftIO getMove
   s <- reaction mi
@@ -45,6 +51,6 @@ gameSession = do
 
 main :: IO ()
 main = do
-  _ <- runStateT gameSession startGame
+  _ <- runStateT (runStateT gameSession Black) startBoard
   return ()
 
--- src/Game.hs
+++ src/Game.hs
@@ -11,16 +11,13 @@ data Game = MkGame { getBoard :: Board, getPlayer :: Player } deriving Show
 startGame :: Game
 startGame = MkGame startBoard Black
 
-play :: (Monad m) => Coordinate -> StateT Game m Bool
+play :: (Monad m) => Coordinate -> StateT Player (StateT Board m) Bool
 play coord  = do
-  (MkGame board player) <- get
-  let (legal, newBoard) = runState (stateMove player coord) board
-  put (MkGame newBoard player)
+  player <- get
+  legal <- lift (stateMove player coord)
+  put player
   _ <- switchPlayer
   return legal
 
-switchPlayer :: (Monad m) => StateT Game m ()
-switchPlayer = do
-  (MkGame board player) <- get
-  put (MkGame board (switch player))
-  return ()
+switchPlayer :: (Monad m) => StateT Player m ()
+switchPlayer = modify switch
