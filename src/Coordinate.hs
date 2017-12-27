bcafbff2bca2dd8439db0524643a2e0cd6b28f86
Olivier Verdier <olivier.verdier@gmail.com>
2017-12-27 17:50:16 +0100

    MakeVector -> MkVector

--- src/Coordinate.hs
+++ src/Coordinate.hs
@@ -12,26 +12,26 @@ instance Arbitrary Coordinate where
     Positive y <- arbitrary
     return (MakeCoordinate x y)
 
-data Vector = MakeVector Int Int deriving (Show)
+data Vector = MkVector Int Int deriving (Show)
 
 
 class Action a where
   plus :: Vector -> a -> a
 
 instance Action Coordinate where
-  plus (MakeVector vi vj) (MakeCoordinate i j) = MakeCoordinate (i+vi) (j+vj)
+  plus (MkVector vi vj) (MakeCoordinate i j) = MakeCoordinate (i+vi) (j+vj)
 
 instance Action Vector where
-  plus (MakeVector i j) (MakeVector vi vj) = MakeVector (i+vi) (j+vj)
+  plus (MkVector i j) (MkVector vi vj) = MkVector (i+vi) (j+vj)
 
 data Direction = N | S | E | W | NW | NE | SE | SW deriving Enum
 
 
 getVector :: Direction -> Vector
-getVector N = MakeVector (-1)  0
-getVector S = MakeVector 1  0
-getVector E = MakeVector 0  1
-getVector W = MakeVector 0  (-1)
+getVector N = MkVector (-1)  0
+getVector S = MkVector 1  0
+getVector E = MkVector 0  1
+getVector W = MkVector 0  (-1)
 getVector NW = plus (getVector N) (getVector W)
 getVector NE = plus (getVector N) (getVector E)
 getVector SE = plus (getVector S) (getVector E)
